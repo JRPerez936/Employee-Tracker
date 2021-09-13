56cbe7cb4bdfe4f99840b72d4585e8873ef62e82
Justin Perez <perezjrp936@gmail.com>
2021-09-12 21:51:13 -0400

    created functions for employee tracker and started working on view departments

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const db = require('./db/connection');
+
+const startEmployeeTracker = () => {
+    console.log(`
+    =================
+    Welcome To Employee Tracker! 
+    =================
+    `);
+    return inquirer.prompt({
+        type:'list',
+        name:'options',
+        message:'What would you like to do?',
+        choices: ['View All Departments','View All Roles','View All Employees','Add a Department','Add a Role','Add a Employee','Update Employee Role']
+    });
+};
+
+const viewDepartment = () => {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, result)=>{
+        if(err) throw err;
+        console.table(result);
+    });
+};
+
+const viewRoles = () => {};
+
+const viewEmployees = () => {};
+
+const addDepartment = () => {};
+
+const addRole = () => {};
+
+const addEmployee = () => {};
+
+const updateRole = () => {};
