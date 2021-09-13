42c9abcfa69f336d38f22e353e118f4f78d2b569
Justin Perez <perezjrp936@gmail.com>
2021-09-12 23:44:48 -0400

    removed personal password from connection.js

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'SuperMari0$unshine2',
+      password: 'password',
       database: 'employees'
     },
     console.log('Connected to the employee database.')
